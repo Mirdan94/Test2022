66a0bb769155ace3ffa66072ed81a21d6a325fe2
Mirdan39 <akinovmirdan@gmail.com>
2022-06-04 19:50:56 +0600

    Работа с Итератором, работа с Листом

--- app/src/main/java/com/example/a1/MainActivity.java
+++ app/src/main/java/com/example/a1/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.a1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.people.PeopleManager;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,11 +17,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+            ArrayList<String> names=new ArrayList<>();
+    names.add("John");
+    names.add("Mike");
+    names.add("Jim");
+    names.add("Vasia");
+        System.out.println(names.get(1)); //распечатка имени из листа
 
+        List<Person> peoples=new ArrayList(); //работа с Листом
+        Person pl=new Person("Meerim",78);
+        peoples.add(pl);
+        peoples.add(new Person("Lisa", 33));
+        peoples.add(new Person("Mike",23));
+        peoples.add(new Person("Mirlan",33));
+        peoples.add(new Person("Jack",21));
+        peoples.add(new Person("Lucie",54));
+        peoples.add(new Person("Scarlet",21));
 
-    }
-
-    public void say (String name) {
-        System.out.println("Ffdfs" + name);
+        Iterator<Person> iter = peoples.iterator(); //работа с Итератором
+        while(iter.hasNext()){
+            Person p = iter.next();
+            System.out.println(p.getName() + "возраст" + p.getAge());
+        }
     }
 }
